22ba782b6ed646b9b069d4f64ec9427d459cc5fb
HoshinoHideki <dondake@yandex.ru>
2022-04-20 17:38:32 +0300

    Some code cleanup.

--- Guesser.py
+++ Guesser.py
@@ -1,7 +1,5 @@
 import random
-import easygui
 import json
-# import text_messages
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -14,47 +12,42 @@ class Card:
 
 def generate_batch(source, number, front, back):
     """
-    This generates a list of Сard objects from the source.
+    This generates a list of "Сard" objects from the source.
     The Сard object has two attributes: "front" and "back".
     Attribute "front" designates which key is the front value.
     Attribute "back" designates which key the back value.
     """
 
     source = random.sample(source, number)
-    batch = list()
+    new_batch = list()
 
     for entry in source:
         # Every iterable is a dict with two entries.
         # Dict key designates whether it is a front or a back of the card.
         # Dict values = values of the attributes.
         card = Card(front=entry[front], back=entry[back])
-        batch.append(card)
-    return batch
+        new_batch.append(card)
+    return new_batch
 
 
-def show_batch(batch):
-    """
-    This will start a sequence of cards for the player.
-    Takes in a dictionary of Card objects with ".front" and ".back" attributes
-    and then shows each front card and then back card.
-    There is no feedback from user yet.
-    """
-        # Front text content:
-    global counter
-    front = batch[counter].front
-    back = batch[counter].back
-    return front, back, counter
+# def show_batch(batch):
+#     """
+#     This will start a sequence of cards for the player.
+#     Takes in a dictionary of Card objects with ".front" and ".back"
+#     attributes and then shows each front card and then back card.
+#     There is no feedback from user yet.
+#     """
+#     global counter
+#     front = batch[counter].front
+#     back = batch[counter].back
+#     return front, back, counter
 
 
-TITLE = "Guesser"
-
-# Load the database
-with open("alphabet.json", "r", encoding="utf-8") as file:
-    database = json.load(file)
-
+# Routing. Move to different module?
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/browse")
 def browse():
@@ -63,66 +56,41 @@ def browse():
 
 @app.route("/train")
 def train():
-    global batch
-    global counter
+    global counter, data
     counter = 0
-    batch = generate_batch(database["alphabet"], 5, "armenian", "english")
-    front, back, counter = show_batch(batch)
-    return render_template("train.html",front=front, back=back, counter=counter, batch=batch)
+    data = generate_batch(database["alphabet"], 5, "armenian", "english")
+    return render_template("train.html",
+                           front=data[counter].front,
+                           back=data[counter].back,
+                           counter=counter,
+                           data=data)
 
 
 @app.route("/train/next")
 def train_next():
-    global counter, batch
+    global counter, data
     counter += 1
-    if counter >= (len(batch)):
-        return "You are done"
+    if counter >= (len(data)):
+        return "You are done"  # TODO: Add a whole page.
     else:
-        front, back, counter = show_batch(batch)
-        return render_template("train.html", front=front, back=back, counter=counter, batch = batch)
+        return render_template("train.html",
+                               front=data[counter].front,
+                               back=data[counter].back,
+                               counter=counter,
+                               data=data)
 
-# Greetings
-# easygui.msgbox(text_messages.hello, TITLE)
 
+# Set globals?
+counter = 0
+data = []
 
+# Load the database
+# TODO: Testing stuff with just armenian alphabet. Possibly expand?
+with open("alphabet.json", "r", encoding="utf-8") as file:
+    database = json.load(file)
+
+# Start frontend
 if __name__ == "__main__":
     app.run(debug=True)
 
-# Main Loop.
-# while True:
-#     mainmenu = easygui.choicebox(msg="Select what you want to do.",
-#                                  title="Language Guesser",
-#                                  choices=["Train", "Browse"])
-#
-#     if mainmenu == "Train":
-#         # TODO:
-#             # Choosing a pool of exercise.
-#             # Choosing a front\back mode.
-#         # Let's ask the player if he wants to practice recognition or recall.
-#         # front_choice = [] #a piece of code that generates a list consisting
-#         # ["english", "armenian"]
-#         # front = easygui.choicebox(
-#         #     text_messages.modeMessage,
-#         #     choices=front_choice,
-#         #     title=TITLE)
-#
-#         # Now Generating a batch of random N cards.
-#         # Maybe ask user to specify how many?
-#         new_batch = generate_batch(database["alphabet"],
-#                                    5, "armenian", "english")
-#
-#         # Show the training batch.
-#         show_batch(new_batch)
-#
-#     if mainmenu == "Browse":
-#         # this shows a list of cards without any functionality.
-#         # to do: choice whether to show english or armenian letters.
-#         alphabet_choices = list()
-#         for item in database["alphabet"]:
-#             alphabet_choices.append(f'{item["armenian"]}: {item["english"]}')
-#         alphabet_lookup = easygui.choicebox("List of cards:",
-#                                             title=TITLE,
-#                                             choices=alphabet_choices,
-#                                             )
-#     if not mainmenu:
-#         break
+
