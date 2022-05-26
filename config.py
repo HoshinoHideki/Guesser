08a9856787940b3b704deea0e5020485435b4cd5
HoshinoHideki <dondake@yandex.ru>
2022-05-26 19:45:40 +0300

    some stuff

--- config.py
+++ config.py
@@ -1,1 +1,28 @@
+from datetime import datetime
+
 DATABASE = "data_v3.json"
+
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+BLANK_CARD = {
+    "key_0": "",
+    "key_1": "",
+    "card_id": "",
+    "key_0_data": {
+        "last_date": "",
+        "next_date": "",
+    },
+    "key_1_data": {
+        "last_date": "",
+        "next_date": "",
+    }
+}
+
+ITEM_STRUCTURE = {
+    "deck":{
+        "languages":
+            ["",""],
+        "cards":[BLANK_CARD,]
+    }
+}
+
